414f2eb28d751095019761fecc93f343376ae9ec
Sorina <chirila_sorina@yahoo.com>
2019-01-31 14:45:52 +0200

    execution time for recursive and nonrecursive fibonacci

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,11 +1,40 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.runners.Parameterized.*;
+
+@RunWith(Parameterized.class)
 
 public class FibonacciTest {
+
+    @Parameters
+    public static Collection<Integer[]> parameters(){
+        return Arrays.asList(new Integer[][]{
+            {0, 1},
+            {1, 1},
+            {2, 2},
+            {3, 3},
+            {4, 5},
+            {5, 8}
+
+        });
+
+        }
+    @Parameter(0)
+    public int index;
+
+    @Parameter(1)
+    public int value;
+
+
 
     private Fibonacci fibonacci;
 
@@ -15,6 +44,18 @@ public class FibonacciTest {
     public void initFibonacci(){
         fibonacci = new Fibonacci();
     }
+
+
+    @Test
+    public void testFibonacciNumberRecursive(){
+        assertEquals(value, fibonacci.computeRecursive(index));
+    }
+
+    @Test
+    public void testFibonacciNumberNonRecursive(){
+        assertEquals(value, fibonacci.computeNonRecursive(index));
+    }
+
 
     @Test(expected = RuntimeException.class)
     public void testWhenFibonacciIsNegativeComputeRecursiveMethodReturnsRuntimeException(){
@@ -74,5 +115,7 @@ public class FibonacciTest {
     public void testWhenFibonacciIndexIsSevenComputeNonRecursiveMethodReturns21(){
         assertEquals(21, fibonacci.computeNonRecursive(7));
     }
+
+
 
 }
